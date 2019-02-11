ce61f1c54c86a14ae8052440f09d3fc4443d478b
Lily Lux <gnabasikat@msoe.edu>
2019-02-10 19:07:07 -0600

    installed angular material, updated UI with it

--- frontend/foo/src/app/app.module.ts
+++ frontend/foo/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatInputModule, MatCardModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +15,10 @@ import { PatientFormComponent } from './patient-form/patient-form.component';
   ],
   imports: [
     ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
     BrowserModule,
     AppRoutingModule
   ],
